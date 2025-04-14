f4e1f237adb02efbfcab8785241f4bc98747ac1f
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-04-14 18:11:02 -0300

    fim das alterações bases de hoje

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Aumenta número no carrinho
+var carrinhoProdutos = document.getElementById('quant')
+var quantidadeCarrinho = 0
+carrinhoProdutos.innerHTML = `<p>${quantidadeCarrinho}</p>`
+
 function comprar() {
-     
+    quantidadeCarrinho++
+    carrinhoProdutos.innerHTML = `<p>${quantidadeCarrinho}</p>`
+}
+
+// Modal
+const modal = document.getElementById('modal')
+
+function mostraModal() {
+    modal.showModal()
+}
+
+function fechaModal () {
+    modal.close()
 }
