6a5fe98b385184db39cf70cb4586bf7fbfc7c3d4
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-04-16 15:35:50 -0300

    começo da lógica do carrinho de comprar 💃

--- script.js
+++ script.js
@@ -18,3 +18,81 @@ function mostraModal() {
 function fechaModal () {
     modal.close()
 }
+
+// Adiciona produtos no carrinho 
+function addControle() {
+    let precoControle = 253.99
+
+    modal.innerHTML +=  `<div class="produto">
+                <section class="img" id="controle"></section>
+                <section class="text">
+                    <h1>PowerStick Pro</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoControle}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addTenis() {
+    let precoTenis = 198.50
+
+    modal.innerHTML += `<div class="produto">
+                <section class="img" id="tenis"></section>
+                <section class="text">
+                    <h1>Runner Z 9000</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoTenis}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addPortatil() {
+    let precoConsole = 315.65
+
+    modal.innerHTML += `<div class="produto">
+                <section class="img" id="console"></section>
+                <section class="text">
+                    <h1>RetroBox Mini</h1>
+                    <h2>Valor:</h2>
+                    <p>${precoConsole}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addBone() {
+    let precoBone = 119.99
+
+    modal.innerHTML += `<div class="produto">
+                <section class="img" id="boné"></section>
+                <section class="text">
+                    <h1>Boné CodeOn</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoBone}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>
+`
+}
