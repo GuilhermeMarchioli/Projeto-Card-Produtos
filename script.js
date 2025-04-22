b6f749a25ed1abcbea0d355adc982b1bdfffbcf8
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-04-22 16:18:17 -0300

    final da responsividade nos  blocos e final do carrinho ( falta o botao deletar)

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function addPortatil() {
                 <section class="text">
                     <h1>RetroBox Mini</h1>
                     <h2>Valor:</h2>
-                    <p>${precoConsole}</p>
+                    <p>R$${precoConsole}</p>
                 </section>
                     <section class="remove">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
@@ -95,4 +95,80 @@ function addBone() {
                 </section>
             </div>
 `
+}
+
+function addHeadset() {
+    let precoHeadset = 261.99
+
+    modal.innerHTML = `<div class="produto">
+                <section class="img" id="fone"></section>
+                <section class="text">
+                    <h1>Headset Sonic Boom X1</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoHeadset}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addTeclado() {
+    var precoTeclado = 229.99
+
+    modal.innerHTML = `<div class="produto">
+                <section class="img" id="teclado"></section>
+                <section class="text">
+                    <h1>Teclado DarkBlade</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoTeclado}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addMouse() {
+    var precoMouse = 119.99
+
+    modal.innerHTML = `<div class="produto">
+                <section class="img" id="mouse"></section>
+                <section class="text">
+                    <h1>Mouse AimMaster V2</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoMouse}</p>
+                </section>
+                    <section class="remove">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
+}
+
+function addNotebook() {
+    var precoNotebook = 1459.99
+
+    modal.innerHTML = `<div class="produto">
+                <section class="img" id="pc"></section>
+                <section class="text">
+                    <h1>Notebook FlashNot</h1>
+                    <h2>Valor:</h2>
+                    <p>R$${precoNotebook}</p>
+                </section>
+                    <section class="remove" onclick="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                </section>
+            </div>`
 }
